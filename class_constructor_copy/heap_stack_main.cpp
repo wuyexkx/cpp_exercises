64998d3dd1d97a2c35fcb850b0a74d2452f8f4d8
wuyexkx <1191468572@qq.com>
2019-07-23 09:28:28 +0800

    heap(堆) and stack(栈) and 生命期

--- class_constructor_copy/heap_stack_main.cpp
+++ class_constructor_copy/heap_stack_main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "clsWithPointerMember.h"
 
-/* heap(堆) and stack(栈) and 生命期
+/* stack(栈) and heap(堆) and 生命期
 --Stack: 是存在于某作用域(scope)的一块内存空间(memory space)。
     例如当你调用函数，函数本身即会形成一个stack用来放置它所接收的参数，以及返回地址。
-    在函数体内声明的变量，其所使用的内存块都取自上诉stack。
-
+    在函数体内声明的变量，其所使用的内存块都取自上述stack。
     作用域内的对象，称为stack object or auto object, 会被自动清理
+    {
+        ...
+    }
 
 
 --Heap: 是指操作系统提供的一块global内存空间，程序可以动态分配从中获得若干区块。
@@ -17,7 +19,7 @@
     {
         static complex c2(1, 2);
     }
-    c2为static obj，其生命在作用域结束之后仍然存在，直到整个程序结束。
+    c2为static obj，其生命在作用域结束之后仍然存在，直到整个程序结束。但只有local可见。
 
 
 ---global object的生命期
@@ -35,11 +37,12 @@
     p1和p2所指的都是heap obj，p1所指空间生命在delete时结束；
     p2在作用域完时结束，但p2所指heap obj仍然存在。作用域之外再也不见p2，再也无法delete p2。内存泄漏。
    ex1：                              ex2:
-    {                                  {
+    {                                  {+
         complex* p1 = new complex;         complex* p2 = complex;
         ...                            }
         delete p1; 
     }
+    内存泄漏：申请的内存空间，在经过某些时间或作用域之后，对其失去控制，以至于无法归还给操作系统。
 
  */
 
