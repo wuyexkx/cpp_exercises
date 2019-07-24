f95b35a43d0f8566bbb423bac3d7ef7f026e3396
wuyexkx <1191468572@qq.com>
2019-07-24 18:03:45 +0800

    class/function template, 解释(T& binding to const T&)

---
+++ class_constructor_copy/namespace.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// 可以写到不同的文件里，最后编译器会组合到一起
+// 防止命名名称与其他冲突
+// namespace std
+// {
+
+// }
+
+// 使用
+// 1. using directive(全部打开)
+    // namespace std; 
+// 2. using declaration(逐条声明)
+    // using std::cout;
+
