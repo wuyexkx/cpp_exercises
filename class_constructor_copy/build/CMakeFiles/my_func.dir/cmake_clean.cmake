914410768fe85b482011e5fa6b2af3bfd0fcedac
wuyexkx <1191468572@qq.com>
2019-07-24 23:57:31 +0800

    composition（复合） delegation（委托） inheritance（继承）

---
+++ class_constructor_copy/build/CMakeFiles/my_func.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_func.dir/src/clsWithPointerMember.cpp.obj"
+  "CMakeFiles/my_func.dir/src/clsWithoutPointerMember.cpp.obj"
+  "libmy_func.pdb"
+  "libmy_func.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_func.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ class_constructor_copy/composition.cpp
@@ -0,0 +1,79 @@
+
+// 类与类之间的三大关系
+
+// ----------------------1.compsiton（复合）----------------------------Adapter
+// 类的定义中包含其他类对象   container(queue) --- component(deque)
+// 构造由内而外，
+// container::container(...): component() {...};
+// container的构造函数 首先调用component的default构造函数，然后才执行自己
+// 编译器默认添加component()，调用的是默认构造函数。如果需要自定义就要像上面一样 写出来并指定合适的参数
+// 析构由外向内，
+// container::~container(...) { ... ~component(); }
+// container的析构函数首先执行自己，然后才component得析构函数
+// 生命周期同步（同时创建消失）
+
+// sizeof: 40
+template<class T>
+class queue{
+protected:
+    deque<T> c;
+};
+
+// sizeof: 16*2 + 4 + 4
+template<class T>
+class deque{
+protected:
+    Itr<T> start;
+    Itr<T> finish;
+    T** map;
+    unsigned int map_size;
+};
+
+// sizeof: 4 * 4
+template<class T>
+struct Itr{
+    T* cur;
+    T* first;
+    T* last;
+    T** node;
+}; 
+
+
+// --------------2.delegation(委托) [composition by reference]-------------Handle/Body(pImpl)
+// 指针相连
+class StringRep;
+class String{
+public:
+    String();
+    String(const char* s);
+    String(const String& str); // 拷贝构造
+    String& operator= (const  String& s); // 拷贝赋值   
+    ~String();
+private:    
+    // 只提供接口，具体实现在外部，外部变动不影响这里
+    StringRep* rep; // handle  [composition by reference] point to implement-pimpl
+};
+
+class StringRep{    // body
+friend class String;
+    StringRep(const char* );
+    ~StringRep();
+    int count;
+    char* rep;
+};
+
+
+// -----------------------3.inheritance(继承)----------------------------
+// 构造由内向外，
+// Derived::Derived(...) Base() {...};
+// Derived的构造函数首先调用Base的default构造函数，然后才执行自己。
+// 
+// 析构由外向内，
+// Derived::~Derived(...) {... ~Base();};
+// Derived的析构函数首先执行自己，然后才调用base的析构函数
+// 
+// Base class的析构函数必须是virtual，否则出现undefined behavior，
+// 当设计的类会被继承，必须设计其析构函数类型为virtual
+
+
+
