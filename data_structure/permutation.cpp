e028f5a0b8f9f3a1dc958be6572e5cec6fb954ae
wuyexkx <1191468572@qq.com>
2019-07-26 18:00:07 +0800

    permutaiton全排列，递归

--- data_structure/permutation.cpp
+++ data_structure/permutation.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
 using namespace std;
 
-template<class T>
-inline void 
-swap1(T& a, T& b)
-{
-    T t = a;
-    a = b;
-    b = t;
-}
-
 // "abc"的排列组合
 void permutations(char p[], const int st, const int end)
 {
-    if(st == end) // 输出一次结果
-    {
-        cout << p << endl;
-    }
-    else 
+    // 每次进入迭代都会判断 是否 第end-1个已经排好
+    if(st == end) { cout << p << endl; } // 如果第end-1个已经排好，那么第end个就不用再排。直接输出
+    else // 
     {
         for(int i=st; i<=end; i++)
         {
-            swap1(p[st], p[i]);
-            permutations(p, st+1, end);
-            swap1(p[st], p[i]);
-        }
+            swap(p[st], p[i]);  // 将 第st个元素 与 其往后第i个元素 交换
+            permutations(p, st+1, end); // ****假设后面的元素已经递归好****
+            swap(p[st], p[i]);  // 还原
+        } 
     }
-
-    // // a开头
-    // swap1(p[0], p[0]);
-    // permutations(p, 1, 2);
-    // swap1(p[0], p[0]);
-    // // b开头
-    // swap1(p[0], p[1]);
-    // permutations(p, 1, 2);
-    // swap1(p[0], p[1]);
-    // // c开头
-    // swap1(p[0], p[2]);
-    // permutations(p, 1, 2);
-    // swap1(p[0], p[2]);
-
 }
+    // ----------帮助理解代码，假设字符 abc-------------
+
+    // 以a开头，假设后面已经排好
+    // swap(p[0], p[0]);
+    // permutations(p, 1, 2); // 假设后面的元素已经递归好
+    // swap(p[0], p[0]);
+
+    // 以b开头，假设后面已经排好
+    // swap(p[0], p[1]); // a跟b交换
+    // permutations(p, 1, 2); 
+    // swap(p[0], p[1]); // 如果 以b开头的 已经迭代输出完，那就复原刚交换的俩
+
+    // 以c开头，假设后面已经排好
+    // swap(p[0], p[2]); // a跟c交换
+    // permutations(p, 1, 2);
+    // swap(p[0], p[2]); // 如果 以c开头的 已经迭代输出完，那就复原刚交换的俩
 
 int main()
 {
-    char p[] = "abcd";
-    permutations(p, 0, 3);
+    char p[] = "abc";
+    permutations(p, 0, 2);
+
+    // 输出结果：
+    // abc
+    // acb
+    // bac
+    // bca
+    // cba
+    // cab
 }
-
-
