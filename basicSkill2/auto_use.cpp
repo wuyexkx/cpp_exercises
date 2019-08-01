b6d0fbd50b2eb4c45c0a6d3344b8d35f04b3d764
wuyexkx <1191468572@qq.com>
2019-08-01 09:53:43 +0800

    auto  Keywords

---
+++ basicSkill2/auto_use.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+/*
+auto 自动推断变量类型, 根据返回值自动推断
+list<string>::iterator ite;
+ite = find(c.brgin(), c.end(), target);
+        |
+        v
+auto ite = find(c.brgin(), c.end(), target);
+*/
+
+int main()
+{
+    return 0;
+}
