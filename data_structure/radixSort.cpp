f43fc2763d13bd8a2e6c217a9bd082a8a18d688a
wuyexkx <1191468572@qq.com>
2019-08-21 10:45:27 +0800

    基数排序, 按照每个位的数来排序.用queue缓存中间结果

--- data_structure/radixSort.cpp
+++ data_structure/radixSort.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 // 基数排序 简单 速度快 多一倍存储空间
 // 基数: 如 10进制的基数为10 8进制的基数为8 对26个字母排序基数就是26 等等
-// 最低位优先, 基数 == 链表的个数
+//      最低位优先, 基数 == 链表的个数
+// 原理:
+//  每次按照数据每个位的数依次排序, 个位 十位 百位...
+//  将中间结果存放在对应的10个容器中 (queue, FIFO)
 
 void print(const list<int>& l)
 {
@@ -39,52 +42,45 @@ tuple<int, int> digits(const list<T>& l)
 template<typename T>
 void radixSort(list<T>& l)
 {
+    // 得到该链表 数据总数 和 最大数的位数
     auto t = digits(l);
-    cout << "\nall nums: " << get<0>(t) << "  max digits: " << get<1>(t) <<  endl;
+    cout << "\nnums: " << get<0>(t) << "  max digits: " << get<1>(t) <<  endl;
 
     list<T> lbuf[10]; // 十进制数,需要10个链表 存放个.十.百.千.位 比较的大小结果
-    int ds = 10;      // 取出对应位的数字, 比较每一位时, 都对ds取余 ds为10 100 1000... 
+    int ds = 1;       // 取出对应位的数字, 比较每一位时, 都对ds取整 ds为1 10 100... 然后对10取余
     for( ; get<1>(t)>0; get<1>(t)--) // n位 排序n次
     {
         // 对应某位的数,存放在缓存中
         auto it = l.begin(); // 比较完某一位后l被改变,需要重新指向
         for( ; it != l.end(); it++)  // nums个数据
         {
-            lbuf[*it % ds].push_back(*it);
+            lbuf[*it / ds % 10].push_back(*it); // 按照 对应位数字 把 该数据放入 对应缓存链表
         }
 
         // 从缓存中取出按某位排序后的数据, 放回原链表
-        int n = 0;
+        int n = 0; // 缓存链表索引
         l.clear(); // 清空原链表
         while(n < 10) // 从缓存链表取出数据 刷新原链表
         {
             while(!lbuf[n].empty()) // 清空某个缓存链表
             {
-                l.push_back(lbuf[n].front());
-                lbuf[n].pop_front();
+                l.push_back(lbuf[n].front()); // 取出数据 保存到 l
+                lbuf[n].pop_front();          // pop掉该缓存
             }
-            n++;
+            n++; // 缓存链表索引
         }
-        print(l);
-        ds *= 10;
+        ds *= 10;// 取整自乘, 1, 10, 100, 1000...
     }
 }
 
 
 int main()
 {
-    list<int> l{2, 51}; // 10个数
-    
+    list<int> l{54, 23, 1002, 999, 2, 19, 51, 41, 22, 4}; // 10个数
     print(l);
     
     radixSort(l);
     print(l);
-
-    // cout << l.front() << endl;
-    // l.pop_front();
-    // it = l.begin();
-    // while(it != l.end()) { cout << *it << " "; it++; } cout << endl;
-
 
     return 0;
 }
