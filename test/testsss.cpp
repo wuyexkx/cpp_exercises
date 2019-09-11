2bec0ec787ff4fc6d56ae6de2716e7b225c08d27
wuyexkx <1191468572@qq.com>
2019-09-11 14:45:18 +0800

    循环顺序队列，数组实现

--- test/testsss.cpp
+++ test/testsss.cpp
@@ -20,12 +20,12 @@ void merge(vector<int>& nums, const int st1, const int st2, const int end)
     for(int i=st1; i<=end; i++)
         nums[i] = temp[i-st1];
 }
-void test(vector<int>& nums, const int left, const int right)
+void sortmerge(vector<int>& nums, const int left, const int right)
 {
     if(left >= right) return;
     int mid = (left + right) / 2;
-    test(nums, left, mid);
-    test(nums, mid+1, right);
+    sortmerge(nums, left, mid);
+    sortmerge(nums, mid+1, right);
     merge(nums, left, mid+1, right);
 
 }
@@ -35,7 +35,7 @@ int main()
     vector<int> v1{3,14,5,2,44,32,-2,-10, 0, 30};
     vector<int> v2{1,3,5,2,4,6};
 
-    test(v1, 0, 9);
+    sortmerge(v1, 0, 9);
     for(auto v : v1)
         cout << v << " ";
     cout << endl; 
