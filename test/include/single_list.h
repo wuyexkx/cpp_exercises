9129c374b9d5cb9b48e507c9b047921aa5dc6a7a
wuyexkx <1191468572@qq.com>
2019-09-14 22:40:55 +0800

    链表反转没写完

--- test/include/single_list.h
+++ test/include/single_list.h
@@ -17,7 +17,7 @@ class List
 {
 public:
     List(): first(nullptr) { }
-    ~List() // 为什么会出问题，不清楚
+    ~List() // windows下为什么会出问题，不清楚
     {
         while(!isEmpty())
         {
@@ -30,6 +30,72 @@ public:
         Node<T>* newNode = new Node<T>(item);
         newNode->link = first;
         first = newNode;
+    }
+    T Front() const
+    {
+        if(isEmpty()) 
+        {
+            std::cout << "empty" << std::endl;
+            return -1;
+        }
+        return first->data;
+    }
+    void Pop_front()
+    {
+        if(isEmpty()) 
+        {
+            std::cout << "empty!" << std::endl;
+            return;
+        }
+        delete first;
+        first = first->link;
+    }
+    void Delete(const T& item)
+    {
+        if(isEmpty())
+        {
+            std::cout << "empty!" << std::endl;
+            return;
+        } 
+        Node<T>* crt = first;
+        Node<T>* prev;
+        while(crt && crt->data != item)
+        {
+            prev = crt;
+            crt = crt->link;
+        } 
+        if(crt) // 找到了
+        {
+            delete crt;
+            if(crt == first) first =  crt->link;
+            else prev->link = crt->link;
+        }
+    }
+    void Invert()
+    {
+        auto p = first;
+        Node<T>* q = nullptr;
+        while(p)
+        {
+            q->link = p;
+            p = p->link;
+        }
+    }
+    void print() const
+    {
+        if(isEmpty())
+        {
+            std::cout << "empty!" << std::endl;
+            return;
+        } 
+        auto p = first;
+        while(p)
+        {
+            std::cout << p->data;
+            if(p->link) std::cout << "->";
+            p = p->link;
+        }
+        std::cout << std::endl;
     }
     bool isEmpty() const { return first == nullptr; }
 
--- test/single_list.cpp
+++ test/single_list.cpp
@@ -5,10 +5,19 @@ using namespace std;
 int main()
 {
     List<int> l;
-    cout <<l.isEmpty() << endl;
     l.Push_front(10);
     l.Push_front(11);
-    cout <<l.isEmpty() << endl;
+    l.Push_front(12);
+    l.Push_front(13);
+    l.print();
 
+    l.Delete(10);
+    l.print();
+    l.Pop_front();
+    l.Delete(12);
+    l.print();
+    l.Pop_front();
+    l.print();
+    
     return 0;
 }
